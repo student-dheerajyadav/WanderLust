970dcfd95848cedae3df5ee69536638817af8cd8
Dheeraj Yadav <yadavdheeraj16840@gmial.com>
2026-08-28 11:46:23 +0530

    listings  new Data

--- app.js
+++ app.js
@@ -1,14 +1,69 @@
 const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
+const listing=require("./models/listing.js")
+const path=require("path");
+app.use(express.urlencoded({extended:true}));
+app.set("views",path.join(__dirname,"views"));
+app.set("view engine","ejs");
 main().then((res)=>{
     console.log("connected to mongoDb dataBase");
 }).catch((err)=>{
     console.log(err);
 })
 async function main(){
-    mongoose.connect("mongodb://127.0.0.1.12017//wonderLust");
+    mongoose.connect("mongodb://127.0.0.1:27017/wonderLust");
 }
+
+// app.get("/testListing",async(req,res)=>{
+//     let smapleListing=new listing({
+//         title:"My new Villa",
+//         description:"By the beach",
+//         price:1200,
+//         loaction:"Calangute,goa",
+//         county:"india",
+//     });
+//    await smapleListing.save();
+//    console.log("Sample was saved");
+//    res.send("Successfully testing");
+// });
+
+
+//new Routes
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs");
+})
+
+//create route
+app.post("/listings",async(req,res)=>{
+    // let{title,description,image,price,location,country}=req.body;
+    const newListing= new listing(req.body.listing);
+    await newListing.save();
+res.redirect("/listings");
+})
+
+//show routes
+app.get("/listings/:id",async(req,res)=>{
+    let{id}=req.params;
+    const list= await listing.findById(id);
+    res.render("listings/show.ejs",{list});
+
+
+
+})
+
+
+
+
+//index Routes
+app.get("/listings",async(req,res)=>{
+  const allListings= await listing.find({});
+  res.render("listings/index.ejs",{allListings});
+       
+
+
+})
+
 
 app.get("/",(req,res)=>{
     res.send("connected SuccessFully")
--- models/listing.js
+++ models/listing.js
@@ -1,17 +1,31 @@
-const mongoose=require("mongoose");
-const schema=mongoose.Schema;
-const listingSchema=new schema({
-    title:{
-        type:String,
-        required:true,
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+
+  description: String,
+
+  image: {
+    filename: {
+      type: String,
+      default: "listingimage",
     },
-    description:String,
-    image:String,
-    price:Number,
-    location:String,
-    country:String,
-})
-const Listing=new mongoose.model("Listing",listingSchema);
-modules.export=Listing;
+    url: {
+      type: String,
+      default:
+        "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    },
+  },
 
+  price: Number,
+  location: String,
+  country: String,
+});
 
+const Listing = mongoose.model("Listing", listingSchema);
+
+module.exports = Listing;
