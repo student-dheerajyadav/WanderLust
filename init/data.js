a0216b33e1404cb959aa92bf99644ad757427f40
Dheeraj Yadav <yadavdheeraj16840@gmial.com>
2026-09-20 00:24:28 +0530

    phase 1 of the project

--- init/data.js
+++ init/data.js
@@ -23,18 +23,18 @@ const sampleListings = [
     location: "New York City",
     country: "United States",
   },
-  {
-    title: "Mountain Retreat",
-    description:
-      "Unplug and unwind in this peaceful mountain cabin. Surrounded by nature, it's a perfect place to recharge.",
-    image: {
-      filename: "listingimage",
-      url: "https://images.unsplash.com/photo-1571896349842-33c89424de2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aG90ZWxzfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
-    },
-    price: 1000,
-    location: "Aspen",
-    country: "United States",
+ {
+  title: "Mountain Retreat",
+  description:
+    "Unplug and unwind in this peaceful mountain cabin. Surrounded by nature, it's a perfect place to recharge.",
+  image: {
+    filename: "listingimage",
+    url: "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?auto=format&fit=crop&w=800&q=60",
   },
+  price: 1000,
+  location: "Aspen",
+  country: "United States",
+},
   {
     title: "Historic Villa in Tuscany",
     description:
